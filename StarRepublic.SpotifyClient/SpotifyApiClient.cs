b14d9a26c540823e9f86a8e2aee1f7b98efaa29b
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-12-17 22:55:13 +0100

    Add project from React.js template

--- StarRepublic.SpotifyClient/SpotifyApiClient.cs
+++ StarRepublic.SpotifyClient/SpotifyApiClient.cs
@@ -9,8 +9,8 @@ namespace StarRepublic.SpotifyClient
 {
     public sealed class SpotifyApiClient
     {
-        private const string ClientId = "996d0037680544c987287a9b0470fdbb";
-        private const string ClientSecret = "5a3c92099a324b8f9e45d77e919fec13";
+        private const string ClientId = "302efa1fb6d24e659ed0554d3733e888";
+        private const string ClientSecret = "8fdd72a5335d46459f27009d50ab9f58";
 
         private const string BaseUrl = "https://api.spotify.com/";
 
