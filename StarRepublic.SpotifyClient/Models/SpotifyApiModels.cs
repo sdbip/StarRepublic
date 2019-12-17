8e8b8db7f78f9a96881ad7d41d718d51a435504d
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-12-17 20:28:09 +0100

    Add StarRepublis code from GitHub
    Add to a Standard Library instead of .Net Framework.

---
+++ StarRepublic.SpotifyClient/Models/SpotifyApiModels.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+/* Hint: Use http://jsonutils.com/ to generate models from JSON responses from the API. */
+
+namespace StarRepublic.SpotifyClient.Models
+{
+	public class SearchArtistResponse
+    {
+        [JsonProperty("artists")]
+        public SearchArtistCollection Artists { get; set; }
+    }
+
+    public class SearchArtistCollection
+    {
+        [JsonProperty("href")]
+        public string Href { get; set; }
+
+        [JsonProperty("items")]
+        public IList<Artist> Items { get; set; }
+
+        [JsonProperty("limit")]
+        public int Limit { get; set; }
+
+        [JsonProperty("next")]
+        public object Next { get; set; }
+
+        [JsonProperty("offset")]
+        public int Offset { get; set; }
+
+        [JsonProperty("previous")]
+        public object Previous { get; set; }
+
+        [JsonProperty("total")]
+        public int Total { get; set; }
+    }
+
+    public class Artist
+    {
+        [JsonProperty("external_urls")]
+        public ExternalUrls ExternalUrls { get; set; }
+
+        [JsonProperty("genres")]
+        public IList<object> Genres { get; set; }
+
+        [JsonProperty("href")]
+        public string Href { get; set; }
+
+        [JsonProperty("id")]
+        public string Id { get; set; }
+
+        [JsonProperty("images")]
+        public IList<Image> Images { get; set; }
+
+        [JsonProperty("name")]
+        public string Name { get; set; }
+
+        [JsonProperty("popularity")]
+        public int Popularity { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; }
+
+        [JsonProperty("uri")]
+        public string Uri { get; set; }
+    }
+
+    public class ExternalUrls
+    {
+        [JsonProperty("spotify")]
+        public string Spotify { get; set; }
+    }
+
+    public class Image
+    {
+
+        [JsonProperty("height")]
+        public int Height { get; set; }
+
+        [JsonProperty("url")]
+        public string Url { get; set; }
+
+        [JsonProperty("width")]
+        public int Width { get; set; }
+    }
+}
---
+++ StarRepublic.SpotifyClient/SpotifyAuthClientCredentialsHttpMessageHandler.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Runtime.Caching;
+using System.Security.Authentication;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using StarRepublic.SpotifyClient.Models;
+
+namespace StarRepublic.SpotifyClient
+{
+	public class SpotifyAuthClientCredentialsHttpMessageHandler : DelegatingHandler
+    {
+        private const string AuthenticationEndpoint = "https://accounts.spotify.com/api/token";
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+
+        public SpotifyAuthClientCredentialsHttpMessageHandler(string clientId, string clientSecret, HttpMessageHandler httpMessageHandler) : base(httpMessageHandler)
+        {
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+        }
+
+        protected override async Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request.Headers.Authorization == null)
+            {
+                request.Headers.Authorization = new AuthenticationHeaderValue("Bearer", await GetAuthenticationTokenAsync());
+            }
+
+            return await base.SendAsync(request, cancellationToken);
+        }
+
+        private async Task<string> GetAuthenticationTokenAsync()
+        {
+            var cacheKey = "SpotifyWebApiSession-Token" + _clientId;
+
+            if (MemoryCache.Default.Get(cacheKey) is string token) return token;
+
+            var timeBeforeRequest = DateTime.Now;
+
+            var response = await GetAuthenticationTokenResponse();
+
+            token = response?.AccessToken;
+            if (token == null)
+                throw new AuthenticationException("Spotify authentication failed");
+
+            var expireTime = timeBeforeRequest.AddSeconds(response.ExpiresIn);
+
+            MemoryCache.Default.Set(cacheKey, token, new DateTimeOffset(expireTime));
+            return token;
+        }
+
+        private async Task<AuthenticationResponse> GetAuthenticationTokenResponse()
+        {
+            var client = new HttpClient();
+
+            var content = new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("grant_type", "client_credentials")
+                //new KeyValuePair<string, string>("scope", "")
+            });
+
+            var authHeader = BuildAuthHeader();
+
+            var requestMessage = new HttpRequestMessage(HttpMethod.Post, AuthenticationEndpoint);
+            requestMessage.Headers.Authorization = new AuthenticationHeaderValue("Basic", authHeader);
+            requestMessage.Content = content;
+
+            var response = await client.SendAsync(requestMessage);
+            var responseString = await response.Content.ReadAsStringAsync();
+
+            var authenticationResponse = JsonConvert.DeserializeObject<AuthenticationResponse>(responseString);
+            return authenticationResponse;
+        }
+        private string BuildAuthHeader()
+        {
+            return Base64Encode(_clientId + ":" + _clientSecret);
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = Encoding.UTF8.GetBytes(plainText);
+            return Convert.ToBase64String(plainTextBytes);
+        }
+    }
+}
+
