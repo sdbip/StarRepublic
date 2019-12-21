e09121a9e51bf17e260f3f78d8c4a4726ff3949d
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-12-21 06:40:48 +0100

    Add fake search results

--- SpotifyRecommendations/ClientApp/src/components/Home.js
+++ SpotifyRecommendations/ClientApp/src/components/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { DropdownItem, DropdownMenu, DropdownToggle, Input, InputGroup, InputGroupButtonDropdown, Label } from 'reactstrap';
+import { Button, DropdownItem, DropdownMenu, DropdownToggle, Fade, Form, Input, InputGroup, InputGroupButtonDropdown, Label, ListGroup, ListGroupItem } from 'reactstrap';
 
 const sampleArtists = [
 	'Bon Jovi',
@@ -11,24 +11,40 @@ const sampleArtists = [
 
 export const Home = () => {
 	const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [resultsVisible, setResultsVisible] = useState(false);
 	const [searchTerm, setSearchTerm] = useState("");
 
-	const toggle = () => {
+	const toggleDropDown = () => {
 		setDropdownOpen(!dropdownOpen);
-	}
+	};
+
+	const search = () => {
+		setResultsVisible(true);
+	};
 
 	return (
-		<InputGroup>
-			<Label for="search-term">Search for...</Label>
-			<Input type="text" name="searchTerm" id="search-term" value={searchTerm} />
-			<InputGroupButtonDropdown addonType="append" isOpen={dropdownOpen} toggle={toggle}>
-				<DropdownToggle caret>
-					Sample searches
-				</DropdownToggle>
-				<DropdownMenu>{sampleArtists.map(name =>
-					<DropdownItem onClick={() => setSearchTerm(name)}>{name}</DropdownItem>)}
-				</DropdownMenu>
-			</InputGroupButtonDropdown>
-		</InputGroup>
+		<Form>
+			<InputGroup>
+				<Label for="search-term">Search for...</Label>
+				<Input type="text" name="searchTerm" id="search-term" value={searchTerm} />
+				<InputGroupButtonDropdown addonType="append" isOpen={dropdownOpen} toggle={toggleDropDown}>
+					<DropdownToggle caret>
+						Sample searches
+					</DropdownToggle>
+					<DropdownMenu>{sampleArtists.map(name =>
+						<DropdownItem onClick={() => setSearchTerm(name)}>{name}</DropdownItem>)}
+					</DropdownMenu>
+				</InputGroupButtonDropdown>
+			</InputGroup>
+			<Button color="primary" onClick={search}>Search</Button>
+			<Fade in={!resultsVisible} tag="h5" className="mt-3">
+				Search for your favourite artists and they will appear here.
+			</Fade>
+			<Fade in={resultsVisible} tag="h5" className="mt-3">
+				<ListGroup>{sampleArtists.map(artist =>
+					<ListGroupItem>{artist}</ListGroupItem>)}
+				</ListGroup>
+			</Fade>
+		</Form>
 	);
 }
