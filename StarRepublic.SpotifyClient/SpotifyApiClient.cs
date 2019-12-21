2a2785cf74866091c91adb71736c8a5c442d5728
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-12-21 07:49:57 +0100

    Add StarRepublic code from GitHub
    Add to a Standard Library instead of .Net Framework.

---
+++ StarRepublic.SpotifyClient/SpotifyApiClient.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Flurl;
+using Newtonsoft.Json;
+using StarRepublic.SpotifyClient.Models;
+
+namespace StarRepublic.SpotifyClient
+{
+	public class SpotifyApiClient
+	{
+		private const string ClientId = "996d0037680544c987287a9b0470fdbb";
+		private const string ClientSecret = "5a3c92099a324b8f9e45d77e919fec13";
+
+		protected const string BaseUrl = "https://api.spotify.com/";
+
+		private static HttpClient GetDefaultClient()
+		{
+			var authHandler = new SpotifyAuthClientCredentialsHttpMessageHandler(
+				ClientId,
+				ClientSecret,
+				new HttpClientHandler());
+
+			var client = new HttpClient(authHandler)
+			{
+				BaseAddress = new Uri(BaseUrl)
+			};
+
+			return client;
+		}
+
+		public async Task<SearchArtistResponse> SearchArtistsAsync(string artistName, int? limit = null, int? offset = null)
+		{
+			var client = GetDefaultClient();
+
+			var url = new Url("/v1/search")
+						.SetQueryParam("q", artistName)
+						.SetQueryParam("type", "artist");
+
+			if (limit != null)
+				url = url.SetQueryParam("limit", limit);
+
+			if (offset != null)
+				url = url.SetQueryParam("offset", offset);
+
+			var response = await client.GetStringAsync(url);
+
+			return JsonConvert.DeserializeObject<SearchArtistResponse>(response);
+		}
+	}
+}
