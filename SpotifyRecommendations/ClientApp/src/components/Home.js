e5272c96ddba8a219f3e00e9d0d0e81ed2f9315a
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-12-21 06:09:48 +0100

    Replace Home content with a search field
    … including a dropdown menu that selects sample searches

--- SpotifyRecommendations/ClientApp/src/components/Home.js
+++ SpotifyRecommendations/ClientApp/src/components/Home.js
@@ -1,20 +1,30 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
+import { DropdownItem, DropdownMenu, DropdownToggle, Input, InputGroup, InputGroupButtonDropdown, Label } from 'reactstrap';
 
-export class Home extends Component {
-  static displayName = Home.name;
+export const Home = () => {
+	const [dropdownOpen, setDropdownOpen] = useState(false);
+	const [searchTerm, setSearchTerm] = useState("");
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, Music Lovers!</h1>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+	const toggle = () => {
+		setDropdownOpen(!dropdownOpen);
+	}
+
+	return (
+		<InputGroup>
+			<Label for="search-term">Search for...</Label>
+			<Input type="text" name="searchTerm" id="search-term" value={searchTerm} />
+			<InputGroupButtonDropdown addonType="append" isOpen={dropdownOpen} toggle={toggle}>
+				<DropdownToggle caret>
+					Sample searches
+        </DropdownToggle>
+				<DropdownMenu>
+					<DropdownItem onClick={() => setSearchTerm('Bon Jovi')}>Bon Jovi</DropdownItem>
+					<DropdownItem onClick={() => setSearchTerm('Bonnie Tyler')}>Bonnie Tyler</DropdownItem>
+					<DropdownItem onClick={() => setSearchTerm('Eric Clapton')}>Eric Clapton</DropdownItem>
+					<DropdownItem onClick={() => setSearchTerm('Janis Joplin')}>Janis Joplin</DropdownItem>
+					<DropdownItem onClick={() => setSearchTerm('Whitesnake')}>Whitesnake</DropdownItem>
+				</DropdownMenu>
+			</InputGroupButtonDropdown>
+		</InputGroup>
+	);
 }
