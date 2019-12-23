d1ca6d38466121d59818ebdf8f6b8ea7759051b2
Johan Segerfeldt <johan.segerfeldt@me.com>
2019-12-23 08:12:51 +0100

    Remove WeatherForecast sample controller

--- SpotifyRecommendations/ClientApp/src/App.js
+++ SpotifyRecommendations/ClientApp/src/App.js
@@ -3,7 +3,6 @@ import { Route } from 'react-router';
 import { Layout } from './components/Layout';
 import { Home } from './components/Home';
 import { About } from './components/About';
-import { FetchData } from './components/FetchData';
 import { Counter } from './components/Counter';
 
 import './custom.css'
@@ -18,7 +17,6 @@ export default class App extends Component {
         <Route exact path='/' component={Home} />
         <Route path='/recommendations' component={Recommendations} />
         <Route path='/counter' component={Counter} />
-        <Route path='/fetch-data' component={FetchData} />
         <Route path='/about' component={About} />
       </Layout>
     );
